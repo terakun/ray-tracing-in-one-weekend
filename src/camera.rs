5c93106772c18d8c277ec7cc24a437e6516b6080
Terasaki Shun <t.tera1217@gmail.com>
2021-08-24 17:06:01 +0900

    finished 11.

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,7 @@
-use super::vec3::{Vec3, Point3};
+use crate::rtweekend;
+
 use super::ray::Ray;
+use super::vec3::{Point3, Vec3};
 
 pub struct Camera {
     origin: Point3,
@@ -9,24 +11,35 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Self {
-        let aspect_ratio = 16.0 / 9.0;
-        let viewport_height = 2.0;
+    // vfov: vertical field-of-view in degrees
+    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
+        let theta = rtweekend::degrees_to_radians(vfov);
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
+
+        let w = Vec3::unit_vector(&(lookfrom - lookat));
+        let u = Vec3::unit_vector(&Vec3::cross(&vup, &w));
+        let v = Vec3::cross(&w, &u);
         let focal_length = 1.0;
 
-        let origin = Point3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0,0.0, focal_length);
+        let origin = lookfrom;
+        let horizontal = viewport_width * u;
+        let vertical = viewport_height * v;
+        let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - w;
 
-        Camera { origin, lower_left_corner, horizontal, vertical }
+        Camera {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray {
             orig: self.origin,
-            dir: self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            dir: self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ mod material;
 use material::{Dielectric, Lambertian, Metal};
 
 mod rtweekend;
-use rtweekend::{random, INFINITY};
+use rtweekend::{random, INFINITY, PI};
 
 fn ray_color(r: &ray::Ray, world: &HittableList, depth: i32) -> Color {
     if depth <= 0 {
@@ -51,6 +51,7 @@ fn main() {
     let max_depth = 50;
 
     // World
+
     let mut world = HittableList {
         objects: Vec::new(),
     };
@@ -58,32 +59,28 @@ fn main() {
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
         100.0,
-        material_ground.clone(),
+        material_ground,
     )));
-
     world.add(Box::new(Sphere::new(
         Point3::new(0.0, 0.0, -1.0),
         0.5,
         material_center,
     )));
-
     world.add(Box::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
         material_left.clone(),
     )));
-
     world.add(Box::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
-        -0.4,
-        material_left.clone(),
+        -0.45,
+        material_left,
     )));
-
     world.add(Box::new(Sphere::new(
         Point3::new(1.0, 0.0, -1.0),
         0.5,
@@ -91,7 +88,13 @@ fn main() {
     )));
 
     // Camera
-    let cam = Camera::new();
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Point3::new(0.0, 1.0, 0.0),
+        20.0,
+        aspect_ratio,
+    );
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
